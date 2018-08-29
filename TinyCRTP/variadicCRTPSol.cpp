ae264f7977ffc6753bfc8ab155cbf0de73c3128e
resema <5762933+resema@users.noreply.github.com>
2018-08-29 16:25:17 +0200

    solution to the crtp riddle

---
+++ TinyCRTP/variadicCRTPSol.cpp
@@ -0,0 +1,166 @@
+#include <iostream>
+#include <set>
+#include <type_traits>
+
+template<typename Derived>
+class ExtraFeature1
+{
+    public:
+        void extraMethod1()
+        {
+            auto derived = static_cast<Derived&>(*this);
+            std::cout << "extra feature 1\n";
+            derived.basicMethod();
+        }
+};
+template<typename Derived>
+class ExtraFeature2
+{
+    public:
+        void extraMethod2()
+        {
+            auto derived = static_cast<Derived&>(*this);
+            std::cout << "extra feature 2\n";
+            derived.basicMethod();
+        }
+};
+template<typename Derived>
+class ExtraFeature3
+{
+    public:
+        void extraMethod3()
+        {
+            auto derived = static_cast<Derived&>(*this);
+            std::cout << "extra feature 3\n";
+            derived.basicMethod();
+        }
+};
+
+//
+// HasFeature
+//  compile checking
+template<typename Derived,
+         template<typename> typename ToCheck,
+         template<typename> typename Current,
+         template<typename> typename... Features>
+constexpr bool HasFeatureOld()
+{
+    if constexpr(std::is_same<ToCheck<Derived>, Current<Derived>>::value)
+        return true;
+    else if constexpr(sizeof...(Features) == 0)
+        return false
+    else
+        return HasFeature<Derived, ToCheck, Features...>();
+}
+
+template<typename Derived,
+         template<typename> typename ToCheck,
+         template<typename> typename... Features>
+         constexpr bool HasFeature()
+{
+    return (std::is_same<ToCheck<Derived>, Features<Derived>>:value || ...);
+}
+
+template<typename Derived, template<typename> typename... Features>
+struct ExtraFeatures : public Features<Derived>...
+{
+};
+
+//
+// JoinTwoExtraFeatures
+template<typename...>
+struct JoinTwoExtraFeatures;
+
+template<typename Derived,
+         template<typename> typename Feature,
+         template<typename> typename... Features1,
+         template<typename> typename... Features2>
+struct JoinTwoExtraFeatures<ExtraFeatures<Derived, Features1...>,
+                            ExtraFeatures<Derived, Feature, Features2...>>
+{
+    using type = typename 
+                std::conditional<
+                    HasFeature<Derived, Feature, Features1...>(),
+                    typename JoinTwoExtraFeatures<
+                        ExtraFeatures<Derived, Features1...>,
+                        ExtraFeatures<Derived, Features2...>
+                    >::type,
+                    typename JoinTwoExtraFeatures<
+                        ExtraFeatures<Derived, Features1..., Feature>,
+                        ExtraFeatures<Derived, Features2...>
+                    >::type
+                >::type;
+};                      
+
+template<typename Derived,
+         template<typename> typename... Features1>
+struct JoinTwoExtraFeatures<ExtraFeatures<Derived, Features1...>,
+                            ExtraFeatures<Derived>>
+{
+    using type = ExtraFeatures<Derived, Features1...>;
+};                  
+
+//
+// JoinExtraFeatures - any number of features
+template<typename...>
+struct JoinExtraFeatures;
+
+template<typename Derived,
+         typename... Packs,
+         template<typename> typename... Features1,
+         template<typename> typename... Features2>
+struct JoinExtraFeatures<ExtraFeatures<Derived, Features1...>,
+                         ExtraFeatures<Derived, Features2...>,
+                         Packs...>
+{
+    using type = typename 
+        JoinExtraFeatures<
+            typename JoinExtraFeatures<
+                ExtraFeatures<Derived, Features1...>,
+                ExtraFeatures<Derived, Features2...>
+            >::type,
+            Packs...
+        >::type;
+};
+
+template<typename Derived,
+         template<typename> typename... Features1,
+         template<typename> typename... Features2>
+struct JoinExtraFeatures<
+            ExtraFeatures<Derived, Features1...>,
+            ExtraFeatures<Derived, Features2...>>
+{
+    using type = typename 
+        JoinTwoExtraFeatures<
+            ExtraFeatures<Derived, Features1...>,
+            ExtraFeatures<Derived, Features2...>
+        >::type;
+};
+
+//
+// ExtraFeaturesXXX
+template<typename Derived>
+using ExtraFeaturesA = ExtraFeatures<Derived, ExtraFeature1, ExtraFeature2>;
+template<typename Derived>
+using ExtraFeaturesB = ExtraFeatures<Derived, ExtraFeature2, ExtraFeature3>;
+template<typename Derived>
+using ExtraFeaturesC = ExtraFeatures<Derived, ExtraFeature1, ExtraFeature3>;
+
+//
+// Class X
+template<template<typename> typename... Skills>
+class X : public JoinExtraFeatures<Skills<X<Skills...>>...>::type
+{
+    public:
+        void basicMethod(){};
+};
+
+int main()
+{
+    using XAB = X<ExtraFeaturesA, ExtraFeaturesB>;
+    
+    XAB x;
+    x.extraMethod1();
+    x.extraMethod2();
+    x.extraMethod3();
+}
--- TinyHana/realworld1.cpp
+++ TinyHana/realworld1.cpp
@@ -3,7 +3,38 @@
 //  Compile with 'wandbox.org'
 
 //
-// example 1:
+// Compile time strings
+#include <cassert>
+#include <type_traits>
+#include <boost/hana.hpp>
+namespace hana = boost::hana;
+
+template<char ...c> struct string {};
+
+template<typename CharT, CharT ...c>
+constexpr string<c...> operator"" _s() { return {}; }
+
+template<char ...c1, char ...c2>
+constexpr auto operator==(string<c1...>, string<c2...>) {
+    return std::is_same<string<c1...>, string<c2...>> {};
+}
+
+template<char ...c1, char ...c2>
+constexpr auto operator+(string<c1...>, string<c2...>) {
+    return string<c1..., c2...> {};
+}
+
+//
+// example 0 : Compile time string testing
+auto hello_world = "hello"_s + " world"_s;
+static_assert(hello_world == "hello world"_s);
+
+int main()
+{
+}
+
+//
+// example 1
 #include <boost/hana.hpp>
 namespace hana = boost::hana;
 
@@ -23,7 +54,7 @@ int main() {
 }
 
 //
-// example 2;
+// example 2 : A Parser
 #include <cassert>
 #include <sstream>
 #include <boost/hana.hpp>
@@ -87,3 +118,101 @@ int main()
   
     assert(data == hana::make_tuple(1, "foo", 3.3));
 }
+
+//
+// example 3 : A Simple Event System
+template<typename ...Events>
+event_system<Events...> make_event_system(Events ...events)
+{
+    return {};
+}
+
+int main()
+{
+    auto events = make_event_system(
+        // "foo"_e = function<void(string)>,
+        "bar"_e = function<void(int)>,
+        "baz"_e = function<void(double)>
+    );
+
+    //
+    // string needs another implementation to know them at compile time
+    
+    // events.on("foo"_e, [](string s) { 
+    //     std::cout << "foo with '" << s << "'!\n"; 
+    // });
+    // events.on("foo"_e, [](string s) {
+    //     std::cout << "foo with '" << s << "' again\n";
+    // });
+    events.on("bar"_e, [](int i) {
+        std::cout << "bar with '" << i << "'!\n";
+    });
+    envents.on("baz"_e, [](double d) {
+        std::cout << "baz with '" << d << "'!\n";
+    });
+    // events.on("unknown"_e, []...); // compiler error!
+
+    // events.trigger("foo"_e, "hello"); // no overhead for event lookup
+    events.trigger("bar"_e, 4);
+    events.trigger("baz"_e, 3.3);
+    // events.trigger("unknown"_e); // compiler error!
+}
+
+//
+// example 4: Type erasure
+#include <cassert>
+#include <type_traits>
+#include <boost/hana.hpp>
+namespace hana = boost::hana;
+
+template<char ...c> struct string {};
+
+template<typename CharT, CharT ...c>
+constexpr string<c...> operator"" _s() 
+{ 
+    return {}; 
+}
+
+template<char ...c1, char ...c2>
+constexpr auto operator==(string<c1...>, string<c2...>) {
+    return std::is_same<string<c1...>, string<c2...>> 
+{};
+}
+
+template<char ...c1, char ...c2>
+constexpr auto operator+(string<c1...>, string<c2...>) {
+    return string<c1..., c2...> {};
+}
+
+//
+struct HasArea : decltype(trait(
+    "area"_s = std::function<double(self const&)>
+)) 
+{};
+
+//
+template<typename Signature>
+constexpr hana::basic_type<Signature> function 
+{};
+
+struct self;
+
+template<char ...c>
+struct string {
+    template<typename Signature>
+    constexpr auto operator=(Signature sig) const {
+        return hana::make_pair(*this, sig);
+    }
+};
+
+struct Circle
+{
+    double x, y, radius;
+};
+
+template<>
+auto impl<HasArea, Circle> = make_impl(
+    "area"_s = [](Circle const& self) -> double {
+        return 3.1415 * (self.radius * self.radius);
+    }
+);
