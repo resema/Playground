86653403a17cec50973b38bee656e28ff651b6b1
resema <5762933+resema@users.noreply.github.com>
2018-12-04 16:20:20 +0100

    added math function variant return

--- TinyPatternMatching/computeRoot.cpp
+++ TinyPatternMatching/computeRoot.cpp
@@ -2,11 +2,11 @@
 #include <iostream>
 #include <cmath>
 
-template<class... Ts> struct overload : Ts...
+template<class... Ts> struct overloaded : Ts...
 {
     using Ts::operator()...;
 };
-template<class... Ts> overload(Ts...) -> overload<Ts...>;
+template<class... Ts> overloaded(Ts...) -> overloaded<Ts...>;
 
 using VarRoot = std::variant<std::pair<double, double>, double, std::monostate>;
 
@@ -30,11 +30,22 @@ VarRoot computeRoots(double a, double b, double c)
 
 void printResult(const VarRoot &var)
 {
-    std::visit(overload {
+    std::visit(overloaded{
         [](const std::pair<double, double>& arg){
-
+            std::cout << "Two roots found: " << arg.first << ", " << arg.second << std::endl;
+        },
+        [](double arg){
+            std::cout << "One root found: " << arg << std::endl;
+        },
+        [](std::monostate){
+            std::cout << "No real root found." << std::endl;
         }
-    })
+    }, var);
 }
 
 int main()
+{
+    printResult(computeRoots(1, -2, -2));
+    printResult(computeRoots(1, 6, 9));
+    printResult(computeRoots(1, -3, 4));
+}
